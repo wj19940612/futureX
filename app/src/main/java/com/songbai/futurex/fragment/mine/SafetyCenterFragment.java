2938ae721a73cbd25ab2866e304478b2bd092938
yangguangda <514939946@qq.com>
2018-06-05 17:57:11 +0800

    增加初级高级谷歌验证等

--- app/src/main/java/com/songbai/futurex/fragment/mine/SafetyCenterFragment.java
+++ app/src/main/java/com/songbai/futurex/fragment/mine/SafetyCenterFragment.java
@@ -9,7 +9,9 @@ import android.view.ViewGroup;
 
 import com.songbai.futurex.R;
 import com.songbai.futurex.activity.UniqueActivity;
+import com.songbai.futurex.view.IconTextRow;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
@@ -19,6 +21,14 @@ import butterknife.Unbinder;
  * @date 2018/5/30
  */
 public class SafetyCenterFragment extends UniqueActivity.UniFragment {
+    @BindView(R.id.setCashPwd)
+    IconTextRow mSetCashPwd;
+    @BindView(R.id.changeLoginPwd)
+    IconTextRow mChangeLoginPwd;
+    @BindView(R.id.googleAuthenticator)
+    IconTextRow mGoogleAuthenticator;
+    @BindView(R.id.gesturePwd)
+    IconTextRow mGesturePwd;
     private Unbinder mBind;
 
     @Nullable
@@ -43,5 +53,21 @@ public class SafetyCenterFragment extends UniqueActivity.UniFragment {
     public void onDestroyView() {
         super.onDestroyView();
         mBind.unbind();
+    }
+
+    @OnClick({R.id.setCashPwd, R.id.changeLoginPwd, R.id.googleAuthenticator, R.id.gesturePwd})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.setCashPwd:
+                break;
+            case R.id.changeLoginPwd:
+                break;
+            case R.id.googleAuthenticator:
+                UniqueActivity.launcher(this, GoogleAuthenticatorFragment.class).execute();
+                break;
+            case R.id.gesturePwd:
+                break;
+            default:
+        }
     }
 }
