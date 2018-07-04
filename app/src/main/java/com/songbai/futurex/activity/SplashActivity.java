ca515551090edc16b5ccea32f8659dfaeab20509
John Zhang <zxukai@gmail.com>
2018-07-04 15:00:13 +0800

    Add Splash page

---
+++ app/src/main/java/com/songbai/futurex/activity/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.songbai.futurex.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.ImageView;
+
+import com.songbai.futurex.R;
+import com.songbai.futurex.utils.Launcher;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Modified by john on 2018/7/4
+ * <p>
+ * Description:
+ * <p>
+ * APIs:
+ */
+public class SplashActivity extends BaseActivity {
+
+    @BindView(R.id.logo)
+    ImageView mLogo;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+        translucentStatusBar();
+
+        mLogo.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Launcher.with(getActivity(), MainActivity.class).execute();
+                finish();
+            }
+        }, 1500);
+    }
+}
