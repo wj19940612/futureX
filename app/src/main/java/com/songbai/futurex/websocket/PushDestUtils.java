936eb24f82f9cbb483967afeb481be61dfecf72d
John Zhang <zxukai@gmail.com>
2018-07-16 10:53:22 +0800

    Add utils to check push type

---
+++ app/src/main/java/com/songbai/futurex/websocket/PushDestUtils.java
@@ -0,0 +1,25 @@
+package com.songbai.futurex.websocket;
+
+import android.text.TextUtils;
+
+/**
+ * Modified by john on 2018/7/16
+ * <p>
+ * Description: 区分 socket push 类型工具类
+ * <p>
+ * APIs:
+ */
+public class PushDestUtils {
+
+    public static boolean isAllMarket(String dest) {
+        return TopicParam.ALL.equals(dest);
+    }
+
+    public static boolean isSoloMarket(String dest) {
+        return !TextUtils.isEmpty(dest) ? dest.contains(TopicParam.SINGLE) : false;
+    }
+
+    public static boolean isEntrustOrder(String dest) {
+        return "queue:entrust".equals(dest);
+    }
+}
